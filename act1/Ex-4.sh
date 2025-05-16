d9587d7f2a31e689e6cfd39cbd4bc5f7528b37ec
joan1113 <164388090+joan1113@users.noreply.github.com>
2025-05-16 20:03:15 +0200

    Create Ex-4.sh

---
+++ act1/Ex-4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+	echo 'Error: 3 parameter is needed'
+else
+	echo 'Nice '$#''
+fi
+root@juan:/home/juan/Bash/P1# cat Ex-4.sh 
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+	echo 'Error: 3 parameter is needed'
+else
+	echo 'Nice '$#''
+fi
